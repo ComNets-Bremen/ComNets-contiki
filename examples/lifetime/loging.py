a0571e9ee393157d8d1387419596bc4694426d94
Shan <shantanoo_desai@hotmail.com>
2016-06-11 15:08:54 +0200

    python2: should work error in recv though

--- examples/lifetime/loging.py
+++ examples/lifetime/loging.py
@@ -1,48 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-# Python 3 is better in terms of fileIO
-
-from serial import Serial
+import serial
 import csv
 import time
 import logging
 
-# Logging Setup
+logging.basicConfig(filename = 'lifetime.log', level=logging.ERROR)
 
-logging.basicConfig(filename = 'lifetime.log', level=logging.INFO)
 
-# change also check for timeout in the Serial () function 
 
-while (1):
-        data = [];
-	# get data from serial port
-        ser = Serial(
-	# Change the port for usb
-        port='/dev/ttyUSB1',
-	# Change the baud rate
-        baudrate=115200,
-        )
 
-        logging.info("Serial Port opened")
+# get data from serial port
+ser = serial.Serial(port='/dev/ttyUSB1',\
+        baudrate=115200,\
+        timeout = None)
 
-        with open("batterystatus.csv", "a") as csvwriter:
-            csvwriter = csv.writer(csvfile, delimiter=';')
-            logging.info("CSV file opened")
 
-            while True:
-                    try:
-                            data= ser.readline()
-                            if not data:
-                                break
-                            print(data)
-                            data1=time.strftime('%H:%M:%S ') + data
-                            csvwriter.writerow(data1.split())
-                            logging.info("Data written in file")
+# open file
 
-                    except KeyboardInterrupt as e:
-                        raise e
-                        logging.error("Keyboard Interrupt/Timeout Triggered")
-                        print('Timeout: Did not receive data')
-                        pass
-            ser.close()
+with open("batterystatus.csv", "a") as csvFile:
+        csvwriter = csv.writer(csvFile, delimiter=";")
+        logging.info("Opened CSV file")
+        while True:
+                try:
+                        data = [];
+                        data = ser.readline()
+                        # if no data break out of the loop
+                        if not data:
+                            break
+                            logging.error("NO DATA")
+
+                        data1 = time.strftime('%H:%M:%S ') + data
+
+                        csvwriter.writerow(data1.split())
+                        logging.info("Written data in file")
+
+                except KeyboardInterrupt, e:
+                    logging.error("This error occured",str(e))
+                    break
+                    ser.close()
 
